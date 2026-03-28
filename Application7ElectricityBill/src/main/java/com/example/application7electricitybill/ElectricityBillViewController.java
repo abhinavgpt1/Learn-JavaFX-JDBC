749fc78528f7f670e9db55cfddef159410a07d23
abhinavgpt1 <abhinav200002@gmail.com>
2026-03-28 10:35:59 +0530

    App7: added different way to load image, image clearance on Clear, validation of text on the fly using Key Type event, docs updation, redundant actions commented to lighten initialize()

--- Application7ElectricityBill/src/main/java/com/example/application7electricitybill/ElectricityBillViewController.java
+++ Application7ElectricityBill/src/main/java/com/example/application7electricitybill/ElectricityBillViewController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 
 public class ElectricityBillViewController {
 
@@ -29,9 +30,8 @@ public class ElectricityBillViewController {
 
     @FXML // fx:id="imgPrevReadingValidation"
     private ImageView imgPrevReadingValidation; // Value injected by FXMLLoader
-
-    @FXML // fx:id="imgUnitExemptionValidation"
-    private ImageView imgUnitExemptionValidation; // Value injected by FXMLLoader
+    @FXML // fx:id="imgNoBillForUnitsValidation"
+    private ImageView imgNoBillForUnitsValidation; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtCurrentReading"
     private TextField txtCurrentReading; // Value injected by FXMLLoader
@@ -48,17 +48,18 @@ public class ElectricityBillViewController {
     @FXML // fx:id="txtTotal"
     private TextField txtTotal; // Value injected by FXMLLoader
 
-    @FXML // fx:id="txtUnitExemptionLimit"
-    private TextField txtUnitExemptionLimit; // Value injected by FXMLLoader
+    @FXML // fx:id="txtNoBillForUnits"
+    private TextField txtNoBillForUnits; // Value injected by FXMLLoader
 
-    private MutableBoolean isPrevReadingValid; // primitive or Wrapper boolean is immutable when passed in function.
+    // FYI, MutableBoolean != AtomicBoolean
+    private MutableBoolean isPrevReadingValid; // primitive boolean is pass-by-value inside validateReadingAndShowIcon function, hence we need a wrapper class on it.
     private MutableBoolean isCurrentReadingValid;
     private MutableBoolean isUnitExemptionValid;
     private MutableBoolean isDiscountValid;
 
-    // pre-load images for performance
+    // Preload images for performance. FYI, imageView are different, so this image won't be shared among them (as expected).
     private final Image TICK = new Image(getClass().getResourceAsStream("tick.png"));
-    private final Image CROSS = new Image(getClass().getResourceAsStream("cross.png"));
+    private final Image CROSS = new Image(String.valueOf(getClass().getResource("cross.png")));
     private final String MISSING_VALUE = "Please enter a number for %s";
     private final float RATE_PER_KWH = 10;
     void validationAlert(String context){
@@ -91,7 +92,7 @@ public class ElectricityBillViewController {
             validationAlert("Current reading can't be less than prev. reading. \nEnter valid readings.");
             return;
         }
-        float unitsExemptionLimit = Float.parseFloat(txtUnitExemptionLimit.getText());
+        float unitsExemptionLimit = Float.parseFloat(txtNoBillForUnits.getText());
         if (unitsConsumed <= unitsExemptionLimit)
             unitsConsumed = 0;
         float totalBill = unitsConsumed * RATE_PER_KWH;
@@ -102,11 +103,16 @@ public class ElectricityBillViewController {
 
     @FXML
     void doClear(ActionEvent event) {
+        // Note: setText() will trigger listener as well as the Key Type event - this will lead to either tick or cross being set
+        // Since we want neither, set the image as null explicitly
         txtPrevReading.setText("");
-        txtCurrentReading.setText("");
+        imgPrevReadingValidation.setImage(null);
 
-        txtUnitExemptionLimit.setText("0");
-        imgUnitExemptionValidation.setImage(null);
+        txtCurrentReading.setText("");
+        imgCurrentReadingValidation.setImage(null);
+
+        txtNoBillForUnits.setText("0");
+         imgNoBillForUnitsValidation.setImage(null);
 
         txtDiscount.setText("0");
         imgDiscountValidation.setImage(null);
@@ -114,7 +120,7 @@ public class ElectricityBillViewController {
         txtTotal.setText("");
         txtNetBill.setText("");
 
-        // flags get set automatically to true/false because of listener.
+        // Flags & images get set automatically to true/false because of listener / key type event upon setText()
         // System.out.println(isPrevReadingValid.get() + ":" + isCurrentReadingValid.get() + ":" + isUnitExemptionValid.get() + ":" + isDiscountValid.get());
     }
 
@@ -124,42 +130,19 @@ public class ElectricityBillViewController {
         System.exit(0);
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert imgCurrentReadingValidation != null : "fx:id=\"imgCurrentReadingValidation\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert imgDiscountValidation != null : "fx:id=\"imgDiscountValidation\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert imgPrevReadingValidation != null : "fx:id=\"imgPrevReadingValidation\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert imgUnitExemptionValidation != null : "fx:id=\"imgUnitExemptionValidation\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert txtCurrentReading != null : "fx:id=\"txtCurrentReading\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert txtDiscount != null : "fx:id=\"txtDiscount\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert txtNetBill != null : "fx:id=\"txtNetBill\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert txtPrevReading != null : "fx:id=\"txtPrevReading\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert txtTotal != null : "fx:id=\"txtTotal\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
-        assert txtUnitExemptionLimit != null : "fx:id=\"txtUnitExemptionLimit\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+    @FXML
+    void validatePrevReadingOnTheFly(KeyEvent event) {
+        validateReadingAndShowIcon(txtPrevReading.getText(), imgPrevReadingValidation, isPrevReadingValid);
+    }
 
-        // initial interface settings
-        txtUnitExemptionLimit.setText("0");
-        txtDiscount.setText("0");
-        isPrevReadingValid = new MutableBoolean(false);
-        isCurrentReadingValid = new MutableBoolean(false);
-        isUnitExemptionValid = new MutableBoolean(true);
-        isDiscountValid = new MutableBoolean(true);
-
-        txtTotal.setDisable(true);
-        txtNetBill.setDisable(true);
-        txtPrevReading.textProperty().addListener(
-                (observable, oldValue, newValue) -> validateReadingAndShowIcon(newValue, imgPrevReadingValidation, isPrevReadingValid));
-        txtCurrentReading.textProperty().addListener(
-                (observable, oldValue, newValue) -> validateReadingAndShowIcon(newValue, imgCurrentReadingValidation, isCurrentReadingValid));
-        txtUnitExemptionLimit.textProperty().addListener(
-                (observable, oldValue, newValue) -> validateReadingAndShowIcon(newValue, imgUnitExemptionValidation, isUnitExemptionValid));
-        txtDiscount.textProperty().addListener(
-                (observable, oldValue, newValue) -> validateReadingAndShowIcon(newValue, imgDiscountValidation, isDiscountValid));
+    @FXML
+    void validateCurrentReadingOnTheFly(KeyEvent event) {
+        validateReadingAndShowIcon(txtCurrentReading.getText(), imgCurrentReadingValidation, isCurrentReadingValid);
     }
 
     private void validateReadingAndShowIcon(String value, ImageView img, MutableBoolean flag) {
         try {
-            Float.parseFloat(value); // Not Integer.parseInt()
+            Float.parseFloat(value); // Not Integer.parseInt() since reading can be in decimal
             img.setImage(TICK);
             flag.set(true);
         } catch(NumberFormatException n){
@@ -170,5 +153,43 @@ public class ElectricityBillViewController {
             else
                 img.setImage(CROSS);
         }
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert imgCurrentReadingValidation != null : "fx:id=\"imgCurrentReadingValidation\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert imgDiscountValidation != null : "fx:id=\"imgDiscountValidation\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert imgPrevReadingValidation != null : "fx:id=\"imgPrevReadingValidation\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert imgNoBillForUnitsValidation != null : "fx:id=\"imgNoBillForUnitsValidation\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert txtCurrentReading != null : "fx:id=\"txtCurrentReading\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert txtDiscount != null : "fx:id=\"txtDiscount\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert txtNetBill != null : "fx:id=\"txtNetBill\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert txtPrevReading != null : "fx:id=\"txtPrevReading\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert txtTotal != null : "fx:id=\"txtTotal\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+        assert txtNoBillForUnits != null : "fx:id=\"txtNoBillForUnits\" was not injected: check your FXML file 'ElectricityBillView.fxml'.";
+
+        // initial interface settings - redundant, since it's present in fxml
+        // txtNoBillForUnits.setText("0");
+        // txtDiscount.setText("0");
+        // txtTotal.setDisable(true);
+        // txtTotal.setEditable(false);
+        // txtNetBill.setDisable(true);
+        // txtNetBill.setEditable(false);
+        isPrevReadingValid = new MutableBoolean(false);
+        isCurrentReadingValid = new MutableBoolean(false);
+        isUnitExemptionValid = new MutableBoolean(true);
+        isDiscountValid = new MutableBoolean(true);
+
+        // No need for following explicit listener since we have action on Key Typed
+        // txtPrevReading.textProperty().addListener(
+        //         (observable, oldValue, newValue) -> validateReadingAndShowIcon(newValue, imgPrevReadingValidation, isPrevReadingValid));
+        // txtCurrentReading.textProperty().addListener(
+        //         (observable, oldValue, newValue) -> validateReadingAndShowIcon(newValue, imgCurrentReadingValidation, isCurrentReadingValid));
+
+        // Here's manual listener as well
+        txtNoBillForUnits.textProperty().addListener(
+                (observable, oldValue, newValue) -> validateReadingAndShowIcon(newValue, imgNoBillForUnitsValidation, isUnitExemptionValid));
+        txtDiscount.textProperty().addListener(
+                (observable, oldValue, newValue) -> validateReadingAndShowIcon(newValue, imgDiscountValidation, isDiscountValid));
     }
 }
