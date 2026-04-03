3237c66da337fd3cd76bd7e38c50f6462a91fde2
abhinavgpt1 <abhinav200002@gmail.com>
2026-04-04 02:36:23 +0530

    App12: media path fixed, handled warning on media files not found

--- Application12PlayMediaSwitchScreensAndEmail/src/main/java/com/example/application12playmediaswitchscreensandemail/PlayMediaController.java
+++ Application12PlayMediaSwitchScreensAndEmail/src/main/java/com/example/application12playmediaswitchscreensandemail/PlayMediaController.java
@@ -1,5 +1,5 @@
 /**
- * Sample Skeleton for 'PlayMediaSwitchScreensAndEmailView.fxml' Controller Class
+ * Sample Skeleton for 'PlayMediaView.fxml' Controller Class
  */
 
 package com.example.application12playmediaswitchscreensandemail;
@@ -31,38 +31,8 @@ public class PlayMediaController {
     @FXML // fx:id="mdVideoPlayer"
     private MediaView mdVideoPlayer; // Value injected by FXMLLoader
 
-    @FXML
-    void navigateToEmailPage(MouseEvent event) {
-        // if a new window is not needed, then switch scenes on the same stage using txtBody.getScene()
-        try {
-            Parent emailRoot = FXMLLoader.load(getClass().getResource("EmailView.fxml"));
-            Scene emailScene = new Scene(emailRoot);
-            Stage emailStage = new Stage();
-            emailStage.setTitle("Email Orbit");
-            emailStage.setScene(emailScene);
-            emailStage.initModality(Modality.APPLICATION_MODAL); // doesn't let you open another email window
-            emailStage.showAndWait();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-
-    // Tip: singleton audioClip can optimise this event
-    @FXML
-    void playAudioClip(MouseEvent event) {
-        // playing audio using MediaPlayer(heavy library) and not AudioClip (light-weight)
-        URL audioUrl = getClass().getResource("media/audioclip.mp3");
-        if (audioUrl == null) {
-            System.out.println("Audio file not found: " + audioUrl);
-            throw new RuntimeException("Audio file not found: " + audioUrl);
-        }
-        Media audioMedia = new Media(audioUrl.toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(audioMedia);
-        mediaPlayer.play();
-    }
-
-    // Tip: singleton audioClip can optimise this event
+    // Tip: singleton audioClip can optimise this event.
+    // Ref: App7ElecticityBill for one-time image loading - tick and cross.
     @FXML
     void playNotification(MouseEvent event) {
         URL audioUrl = getClass().getResource("media/notification.mp3");
@@ -72,6 +42,19 @@ public class PlayMediaController {
         }
         AudioClip audioClip = new AudioClip(audioUrl.toString());
         audioClip.play();
+    }
+
+    @FXML
+    void playAudioClip(MouseEvent event) {
+        // playing audio using MediaPlayer (advanced library) and not AudioClip (light-weight and basic)
+        URL audioUrl = getClass().getResource("media/audioclip.mp3");
+        if (audioUrl == null) {
+            System.out.println("Audio file not found: " + audioUrl);
+            throw new RuntimeException("Audio file not found: " + audioUrl);
+        }
+        Media audioMedia = new Media(audioUrl.toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(audioMedia);
+        mediaPlayer.play();
     }
 
     @FXML
@@ -87,19 +70,37 @@ public class PlayMediaController {
         mediaPlayer.play();
     }
 
+    @FXML
+    void navigateToEmailPage(MouseEvent event) {
+        // PTR: if a new window is not needed, then switch scenes on the same stage using txtBody.getScene()
+        try {
+            Parent emailRoot = FXMLLoader.load(getClass().getResource("EmailView.fxml"));
+            Scene emailScene = new Scene(emailRoot);
+            Stage stage = new Stage(); // for new window
+            stage.setTitle("Email Orbit");
+            stage.setScene(emailScene);
+            stage.initModality(Modality.APPLICATION_MODAL); // doesn't let you open another email window
+            stage.showAndWait();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert mdVideoPlayer != null : "fx:id=\"mdVideoPlayer\" was not injected: check your FXML file 'PlayMediaSwitchScreensAndEmailView.fxml'.";
+        assert mdVideoPlayer != null : "fx:id=\"mdVideoPlayer\" was not injected: check your FXML file 'PlayMediaView.fxml'.";
 
         // PTR:
         // javafx-media maven dependency from org.openjfx is required here for AudioClip, MediaPlayer and MediaView.
         // * add it in VM Options too along with other modules
         //      --add-modules javafx.controls,javafx.fxml,javafx.media
-        // * to remove warning add
+        // * to remove warning on use of native method add following in VM options
         //  --enable-native-access=javafx.media
 
-        // Future development ideas:
-        // 1. add button to stop audio
-        // 2. add buttons to stop, resume or reset video
+        /**
+         * Further enhancements:
+         * 1. add button to stop audio
+         * 2. add buttons to stop, resume or reset video
+         */
     }
 }
